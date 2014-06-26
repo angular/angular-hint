15827c246866365734f011f72ada5a4fd9028d57
Erin Altenhof-Long <ealtenhof@google.com>
2014-06-26 15:41:33 -0700

    feat(ngHint): make loaded modules configurable

--- load.js
+++ load.js
@@ -3,8 +3,19 @@
 //
 // everything on by default
 
-var moduleNames = ['deedLib', 'controllerTrainer'];
+var defaultModules = ['ngHintDirectives', 'ngHintDom'];
 window.name = 'NG_DEFER_BOOTSTRAP!';
 angular.element(document).ready(function() {
-  angular.resumeBootstrap(moduleNames)
+  var selectedModules;
+  var elts = document.querySelectorAll('[ng-hint]');
+  if(elts.length > 0) {
+    selectedModules = elts[0].attributes['ng-hint'].value.split(' ').map(function(name) {
+      return 'ngHint' + name[0].toUpperCase() + name.substring(1);
+    });
+  }
+  else {
+    selectedModules = defaultModules;
+  }
+  angular.resumeBootstrap(selectedModules)
+
 });
