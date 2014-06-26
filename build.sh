aaf9be1d2e59b9ef6dc2756de4043c1e8d789823
Erin Altenhof-Long <ealtenhof@google.com>
2014-06-26 15:01:14 -0700

    chore(build): update build dependency names

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ cat prefix.js \
     $DEP/angular-hint-dom/$DEP/zone.js/zone.js \
     $DEP/angular-hint-dom/$DEP/dom-interceptor/dom-interceptor.js \
     $DEP/angular-hint-dom/domDecorator.js \
-    $DEP/angular-hint-directive/dd-lib/dd-lib.js \
-    $DEP/angular-hint-directive/decorator.js \
+    $DEP/angular-hint-directives/dd-lib/dd-lib.js \
+    $DEP/angular-hint-directives/decorator.js \
     load.js \
     suffix.js > hint.js
