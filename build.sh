5154ad18f0c35833c053a64167859d61b3a1f55c
Brian Ford <btford@umich.edu>
2014-06-26 14:44:37 -0700

    feat(dom): add dom hinting files to build

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 
 DEP=node_modules
 
-DOM=
-#DIRECTIVE=$DEP/
-
 cat prefix.js \
+    $DEP/angular-hint-dom/$DEP/zone.js/zone.js \
+    $DEP/angular-hint-dom/$DEP/dom-interceptor/dom-interceptor.js \
+    $DEP/angular-hint-dom/domDecorator.js \
     $DEP/angular-hint-directive/dd-lib/dd-lib.js \
     $DEP/angular-hint-directive/decorator.js \
     load.js \
