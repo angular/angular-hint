491a0a71ffd1ec68f97eb2ed0bcd2b9dfae40d02
Erin Altenhof-Long <ealtenhof@google.com>
2014-07-09 10:37:48 -0700

    feat(hintBootstrapping): add inclusive and exclusive hints
    Load hint modules based on inclusive and exclusive commands
    or all modules by default when ng-hint is used.
    
    #6

--- hint.js
+++ hint.js
@@ -6,20 +6,49 @@
 require('angular-hint-dom');
 require('angular-hint-directives');
 
-var defaultModules = ['ngHintDirectives', 'ngHintDom'];
+var allModules = ['ngHintDirectives', 'ngHintDom'];
 
 window.name = 'NG_DEFER_BOOTSTRAP!';
 
 // determine which modules to load and resume bootstrap
 angular.element(document).ready(function() {
   var selectedModules;
-  var elts = document.querySelectorAll('[ng-hint]');
+  var elts;
+  elts = document.querySelectorAll('[ng-hint-include]');
   if(elts.length > 0) {
-    selectedModules = elts[0].attributes['ng-hint'].value.split(' ').map(function(name) {
+    console.log('include');
+    selectedModules = elts[0].attributes['ng-hint-include'].value.split(' ').map(function(name) {
       return 'ngHint' + name[0].toUpperCase() + name.substring(1);
     });
-  } else {
-    selectedModules = defaultModules;
   }
-  angular.resumeBootstrap(selectedModules)
+  else {
+    elts = document.querySelectorAll('[ng-hint-exclude]');
+    if(elts.length > 0) {
+      elts = elts[0].attributes['ng-hint-exclude'].value.split(' ');
+      selectedModules = allModules.filter(function(name) {
+        var notFound = true;
+        elts.forEach(function(element) {
+          if(('ngHint' + element[0].toUpperCase() + element.substring(1)) == name)
+          {
+            notFound = false;
+          }
+        });
+        if(notFound) {
+          return name;
+        }
+      });
+    }
+    else {
+      elts = document.querySelectorAll('[ng-hint]');
+      if(elts.length > 0) {
+        selectedModules = allModules;
+      }
+    }
+  }
+  if(selectedModules != undefined) {
+    angular.resumeBootstrap(selectedModules);
+  }
+  else {
+    angular.resumeBootstrap();
+  }
 });
