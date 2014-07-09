0f95525f69b83857ecce11768ab0ac83d38386f8
Erin Altenhof-Long <ealtenhof@google.com>
2014-07-09 16:38:26 -0700

    refactor(): update module loading logic

--- hint.js
+++ hint.js
@@ -14,29 +14,36 @@ window.name = 'NG_DEFER_BOOTSTRAP!';
 angular.element(document).ready(function() {
   var selectedModules;
   var elts;
-  elts = document.querySelectorAll('[ng-hint-include]');
-  if(elts.length > 0) {
-    console.log('include');
-    selectedModules = elts[0].attributes['ng-hint-include'].value.split(' ').map(function(name) {
+  var includeModules = function(modulesToInclude) {
+    var selected = modulesToInclude.map(function(name) {
       return 'ngHint' + name[0].toUpperCase() + name.substring(1);
     });
+    return selected;
+  };
+
+  var excludeModules = function(modulesToExclude) {
+    var selected = allModules.filter(function(name) {
+      var notFound = true;
+      modulesToExclude.forEach(function(element) {
+        if(('ngHint' + element[0].toUpperCase() + element.substring(1)) == name) {
+          notFound = false;
+        }
+      });
+      if(notFound) {
+        return name;
+      }
+    });
+    return selected;
+  };
+
+  elts = document.querySelectorAll('[ng-hint-include]');
+  if(elts.length > 0) {
+    selectedModules = includeModules(elts[0].attributes['ng-hint-include'].value.split(' '));
   }
   else {
     elts = document.querySelectorAll('[ng-hint-exclude]');
     if(elts.length > 0) {
-      elts = elts[0].attributes['ng-hint-exclude'].value.split(' ');
-      selectedModules = allModules.filter(function(name) {
-        var notFound = true;
-        elts.forEach(function(element) {
-          if(('ngHint' + element[0].toUpperCase() + element.substring(1)) == name)
-          {
-            notFound = false;
-          }
-        });
-        if(notFound) {
-          return name;
-        }
-      });
+      selectedModules = excludeModules(elts[0].attributes['ng-hint-exclude'].value.split(' '));
     }
     else {
       elts = document.querySelectorAll('[ng-hint]');
