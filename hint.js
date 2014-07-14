2aafaf59bd56650207f89b9f318868ee4bfbe01a
Erin Altenhof-Long <ealtenhof@google.com>
2014-07-14 10:21:14 -0700

    fix(ngHint): refactor to remove unintentional test error
    After the refactor of logic in #22 the function loadModules()
    was removed which is necessary for the operation of the
    monkey patching of resumeBootstrap. This refactor
    adds back a function that returns a list of modules
    without calling resumeBootstrap().

--- hint.js
+++ hint.js
@@ -16,7 +16,7 @@ if(window.name === 'NG_DEFER_BOOTSTRAP!') {
   Object.defineProperty(angular, 'resumeBootstrap', {
     get: function() {
       return function(modules) {
-        return(originalResumeBootstrap.call(angular, modules.concat(loadHintModules())))
+        return(originalResumeBootstrap.call(angular, modules.concat(loadModules())));
       }
     },
     set: function(resumeBootstrap) {
@@ -35,14 +35,18 @@ else {
   window.name = 'NG_DEFER_BOOTSTRAP!';
 }
 
-// determine which modules to load and resume bootstrap
-document.addEventListener('DOMContentLoaded', function maybeBootstrap() {
+function maybeBootstrap() {
 
   // we don't know if angular is loaded
   if (!angular.resumeBootstrap) {
     return setTimeout(maybeBootstrap, 1);
   }
 
+  var modules = loadModules();
+  angular.resumeBootstrap(modules);
+}
+
+function loadModules() {
   var modules = [], elt;
 
   if (elt = document.querySelector('[ng-hint-include]')) {
@@ -52,9 +56,11 @@ document.addEventListener('DOMContentLoaded', function maybeBootstrap() {
   } else if (document.querySelector('[ng-hint]')) {
     modules = allModules;
   }
+  return modules;
+};
 
-  angular.resumeBootstrap(modules);
-});
+// determine which modules to load and resume bootstrap
+document.addEventListener('DOMContentLoaded', maybeBootstrap());
 
 
 function excludeModules(modulesToExclude) {
