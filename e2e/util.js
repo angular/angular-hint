9340e459952bd111cabb27b383813c3e607c4c0c
Erin Altenhof-Long <ealtenhof@google.com>
2014-07-25 14:34:19 -0700

    fix(e2e): remove innerHTML patching hack
    Previously an issue with the patching in ngHintDom caused infinite recursion
    when attempting to log messages to a div to be read by protractor. After updating
    the ngHintDom dependency, this hack can be removed.
    
    Closes #24

--- e2e/util.js
+++ e2e/util.js
@@ -1,11 +1,4 @@
 var log = document.getElementById('console');
-var original = Object.getOwnPropertyDescriptor(Element.prototype, 'innerHTML');
 angular.hint.onMessage = function(message) {
-  if(original && typeof original.get === 'function' && typeof original.set == 'function') {
-    Object.defineProperty(log, 'innerHTML', {
-      get: original.get,
-      set: original.set
-    });
-  }
   log.innerHTML += message;
 };
