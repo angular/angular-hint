8c3ce548734fc954312e6d578a7557c2a1272649
Erin Altenhof-Long <ealtenhof@google.com>
2014-07-31 16:40:52 -0700

    chore(hintLog): update to latest hintLog syntax without trailing space

--- hint_test.js
+++ hint_test.js
@@ -8,18 +8,18 @@ describe('angularHint', function() {
     it('should warn if ng-hint is called with unknown options', function() {
       browser.get('includeWrongModuleName/');
       expect(element(by.id('console')).getText())
-          .toBe('Angular Hint: General Module ngHintWrongModuleName could not be found');
+          .toBe('Angular Hint: GeneralModule ngHintWrongModuleName could not be found');
       browser.get('excludeWrongModuleName/');
       expect(element(by.id('console')).getText())
-          .toContain('Angular Hint: General Module ngHintExcludeWrongModuleName could not be found');
+          .toContain('Angular Hint: GeneralModule ngHintExcludeWrongModuleName could not be found');
     });
 
 
     it('should include all modules by ng-hint default', function() {
       browser.get('allHint/');
       //angular-hint-controllers
-      expect(element(by.id('console')).getText()).toContain('Controller best practices is for a ' +
-        'controller name to end with \'Controller\'. Check the name of Hint');
+      expect(element(by.id('console')).getText()).toContain('The best practice is to name ' +
+        'controllers ending with \'Controller\'. Check the name of \'Hint\'');
       //angular-hint-directives
       expect(element(by.id('console')).getText()).toContain('ng-repeat');
       //angular-hint-dom
@@ -56,7 +56,7 @@ describe('angularHint', function() {
     it('should warn if there is no ng-hint attribute', function() {
       browser.get('noHint/');
       expect(element(by.id('console')).getText())
-        .toBe('Angular Hint: General ngHint is included on the page, but is not active because there is no `ng-hint` attribute present');
+        .toBe('Angular Hint: GeneralngHint is included on the page, but is not active because there is no `ng-hint` attribute present');
     });
   });
 });
