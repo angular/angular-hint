d22db989c3e61206a162e8a05548ff4a0d1ca97a
Erin Altenhof-Long <ealtenhof@google.com>
2014-08-05 17:37:27 -0700

    feat(example): create "Angular Best Practices" example app to show off AngularHint messages
    #36

--- example/app.js
+++
@@ -1,9 +0,0 @@
-angular.module('sampleApp', ['ngRoute']).
-      config(['$routeProvider', function($routeProvider) {
-        $routeProvider.when('/example', {
-          controller: 'SampleAppController',
-          controllerAs: 'sampleAppCtrl',
-          templateUrl: 'sampleApp.html'
-        }).
-        otherwise({redirectTo: '/example'});
-  }]);
