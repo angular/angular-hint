302eda7681793b66449a242e57a554d8baea9aae
Michał Gołębiowski <m.goleb@gmail.com>
2014-10-24 17:08:47 +0200

    fix(protractor): Don't specify the selenium JAR location
    Protractor now picks it up by itself.

--- protractor.conf.js
+++ protractor.conf.js
@@ -2,9 +2,7 @@
 
 exports.config = {
   baseUrl: 'http://localhost:8080/e2e/',
-
-  seleniumServerJar: './node_modules/protractor/selenium/selenium-server-standalone-2.42.2.jar',
-
+  
   capabilities: {
     'browserName': 'chrome'
   },
